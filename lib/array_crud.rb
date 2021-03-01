8bc8eade90068b4e5cc667529f5f1a41b56bd170
Jasmine Ryon <jryon12@priorypride.net>
2021-03-01 13:15:50 -0800

    commit tree ec3238c60d029ddf917e48df897b8e220f15860f

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,42 @@
 def create_an_empty_array
-  
+    []    
 end
 
 def create_an_array
-  
+    family = ["mariah", "ariel", "jeanette", "david"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+    array = ["mariah", "ariel", "jeanette", "david"]
+    array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+    array = ["mariah", "ariel", "jeanette", "david"]
+    array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+    array = ["mariah", "ariel", "jeanette", "david", "arrays!"]
+    array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+    array = ["wow", "mariah", "ariel", "jeanette", "david"]
+    array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+    array = ["mariah", "ariel", "am", "jeanette", "david"]
+    array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+    array = ["wow", "mariah", "ariel", "jeanette", "david"]
+    array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+    array = ["mariah", "ariel", "jeanette", "david", "arrays!"]
+    array[4]
 end
